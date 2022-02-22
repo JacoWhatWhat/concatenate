ca0b5daaeccf5491e45e90247daee8b4b16d04fb
Jaco <jacovz87@gmail.com>
2022-02-22 23:36:35 +0000

    Add files via upload

---
+++ concat_scrapes.py
@@ -0,0 +1,23 @@
+import glob, os, subprocess
+
+path = "C:\\Users\\Home\\projects\\helm\\concat_config"
+os.chdir(path)
+
+outfilename = path + "\\compiled-config\\prometheus-additional.yaml"
+
+def concatenate_scrapes():
+  with open(outfilename, 'w') as outfile:
+    outfile.seek(0)
+    for filename in glob.glob('scrape-config\\*.yaml'):
+      if filename == outfilename:
+        continue
+      with open(filename, 'r') as readfile:
+        for line in readfile:
+          if not line.isspace():
+            outfile.write(line)
+
+def create_sectet():
+  os.system("kubectl.exe create secret generic additional-scrape-configs --from-file=" + outfilename + " -oyaml > C:\\Users\\Home\\projects\\helm\\concat_config\\compiled-config\\additional-scrape-configs.yaml")
+
+concatenate_scrapes()
+create_sectet()
